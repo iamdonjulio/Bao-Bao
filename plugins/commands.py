27dc7fe79e138cdee744a044d059f5ed1c9e20c1
Don Julio <39365922+iamdonjulio@users.noreply.github.com>
2021-03-29 18:02:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,13 +243,10 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                   
                     InlineKeyboardButton("About Me", callback_data="about_data")
-                ],
-                [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
                 ]
+               
             ]
         ),
         reply_to_message_id=message.message_id
@@ -265,7 +262,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/iamdonjulio/Bao-Bao")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
